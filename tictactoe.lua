da296c31b0dc39429edd73d811806330bf83f0ed
Dominic Rucker <domruck98@gmail.com>
2023-11-04 21:10:59 -0700

    added check winner function

--- tictactoe.lua
+++ tictactoe.lua
@@ -7,10 +7,10 @@ local board = {}
 -- clear the board table emptying all positions
 -------------------------------------------------
 function clear_board()
-  for i = 1, 3 do
+  for (i = 1, 3) do
     -- new row
     board[i] = {}
-    for j = 1, 3 do
+    for (j = 1, 3) do
       board[i][j] = " "
     end
   end
@@ -22,7 +22,7 @@ end
 function display_board()
   -- loop all rows
   print("   1  2  3 ")
-  for i = 1, 3 do
+  for (i = 1, 3) do
     print(i.." ["..board[i][1].."]["..board[i][2].."]["..board[i][3].."]")
   end
 end
@@ -31,9 +31,9 @@ end
 -- return true or false if the board is full
 -------------------------------------------------
 function board_full()
-  for i = 1, 3 do
-    for j = 1, 3 do
-      if board[i][j] == " " then
+  for (i = 1, 3) do
+    for (j = 1, 3) do
+      if (board[i][j] == " ") then
         return false
       end
     end
@@ -45,7 +45,22 @@ end
 -- function to check winnner and returns X or O
 -------------------------------------------------
 function check_winner()
-  
+   -- check rows
+   if (board[1][1] ~= " " and board[1][1] == board[1][2] and board[1][2] == board[1][3]) then return board[1][1] end
+   if (board[2][1] ~= " " and board[2][1] == board[2][2] and board[2][2] == board[2][3]) then return board[2][1] end
+   if (board[3][1] ~= " " and board[3][1] == board[3][2] and board[3][2] == board[3][3]) then return board[3][1] end
+ 
+   -- check cols
+   if (board[1][1] ~= " " and board[1][1] == board[2][1] and board[2][1] == board[3][1]) then return board[1][1] end
+   if (board[1][2] ~= " " and board[1][2] == board[2][2] and board[2][2] == board[3][2]) then return board[1][2] end
+   if (board[1][3] ~= " " and board[1][3] == board[2][3] and board[2][3] == board[3][3]) then return board[1][3] end
+ 
+   -- check diagonals
+   if (board[1][1] ~= " " and board[1][1] == board[2][2] and board[2][2] == board[3][3]) then return board[1][1] end
+   if (board[1][3] ~= " " and board[1][3] == board[2][2] and board[2][2] == board[3][1]) then return board[1][3] end
+ 
+   -- if there is no winner
+   return nil
 end
 
 -------------------------------------------------
@@ -67,13 +82,13 @@ while not game_over do
   local col_to_play = io.read("*n")
 
   -- prevent the user to select a position that is already taken
-  if board[row_to_play][col_to_play] ~= " " then
+  if (board[row_to_play][col_to_play] ~= " ") then
     print("Invalid! Please select an empty position.")
   else
     board[row_to_play][col_to_play] = player
 
     -- the current player if based on odd/even move
-    if move % 2 == 0 then
+    if (move % 2 == 0) then
       player = "X"
     else
       player = "O"
@@ -84,11 +99,11 @@ while not game_over do
     -- check if there is a winner
     local winner = check_winner()
 
-    if winner ~= nil then
+    if (winner ~= nil) then
       display_board()
       print("Game Over! The winner is "..winner)
       game_over = true
-    elseif board_full() then
+    elseif (board_full()) then
       print("Game Over! Tie.")
       game_over = true
     end
